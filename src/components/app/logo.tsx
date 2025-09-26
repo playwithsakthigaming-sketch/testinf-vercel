3b1882a583d3a3e736d2ea75d569418195752549
Play With Sakthi Gaming <playwithsakthigaming@gmail.com>
2025-09-26 12:50:05 +0000

    vtc logo change https://media.discordapp.net/attachments/812969396540145

--- src/components/app/logo.tsx
+++ src/components/app/logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export function Logo({ className, size = 28 }: { className?: string; size?: number }) {
   return (
     <Image
-      src="https://cdn.discordapp.com/attachments/812969396540145694/1420219258834518171/unwatermark_1000037663.gif?ex=68d499c1&is=68d34841&hm=82d9ffaae644401634e086307d8324354e2c4af16d7cca6220ac687671b78a30&"
+      src="https://media.discordapp.net/attachments/812969396540145694/1420219258834518171/unwatermark_1000037663.gif?ex=68d73cc1&is=68d5eb41&hm=da217b31eec1b8f5b51d6c96503a92782ca1d386bb6e1b35148b0bcda1e0c1a2&"
       alt="Tamil Pasanga Logo"
       width={size}
       height={size}
