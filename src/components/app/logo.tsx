1534c74def4774e8eb8e14966fca49fd96ac1863
Play With Sakthi Gaming <playwithsakthigaming@gmail.com>
2025-09-28 12:34:06 +0000

    update logo https://media.discordapp.net/attachments/812969396540145694/

--- src/components/app/logo.tsx
+++ src/components/app/logo.tsx
@@ -4,11 +4,12 @@ import Image from "next/image";
 export function Logo({ className, size = 28 }: { className?: string; size?: number }) {
   return (
     <Image
-      src="https://media.discordapp.net/attachments/1116720480544636999/1274425873201631304/TP_NEW_WB_PNGxxxhdpi.png?ex=68d4d8d5&is=68d38755&hm=b6d4e0e4ef2c3215a4de4fb2f592189a60ddd94c651f96fe04deac2e7f96ddc6&=&format=webp&quality=lossless&width=826&height=826"
+      src="https://media.discordapp.net/attachments/812969396540145694/1420219258834518171/unwatermark_1000037663.gif?ex=68d9dfc1&is=68d88e41&hm=66b812dfa84c8728ba006c020bcf650adff9ebc427961ed5f97050126b9ba9cc&="
       alt="Tamil Pasanga Logo"
       width={size}
       height={size}
       className={cn(className)}
+      unoptimized
     />
   );
 }
