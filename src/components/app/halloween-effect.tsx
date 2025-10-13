4f4ca0ab501dc391fcc26a0b19e6b5e309594192
Play With Sakthi Gaming <playwithsakthigaming@gmail.com>
2025-10-13 15:13:25 +0000

    Halloween pumbkin effect reduce 20 to 5

--- src/components/app/halloween-effect.tsx
+++ src/components/app/halloween-effect.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from 'react';
 
-const HALLOWEEN_ELEMENT_COUNT = 20;
+const HALLOWEEN_ELEMENT_COUNT = 5;
 
 export function HalloweenEffect() {
     const [elements, setElements] = useState<JSX.Element[]>([]);
